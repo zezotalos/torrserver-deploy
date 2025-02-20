cee55f3190c06769a7a4d8e4f1ed93a53a1aed29
zezotalos <zezotalos@outlook.com>
2025-02-20 12:47:30 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,8 +1,14 @@
 import os
 from cryptography.fernet import Fernet
+import base64
 
 encrypted_str = open("script" , 'r').read()
-key = os.environ.get('SECRET_KEY')
+raw_key = os.environ.get('SECRET_KEY')
+if raw_key is None:
+    raise ValueError("SECRET_KEY environment variable is not set.")
+
+key = base64.urlsafe_b64decode(raw_key)  
+
 decrypted = Fernet(key).decrypt(encrypted_str.encode()).decode()
 
 decrypted_file = open("main.py" , 'w')
