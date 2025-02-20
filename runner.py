f82498f4037bf34f642c560594de1e18a025bf47
zezotalos <zezotalos@outlook.com>
2025-02-20 13:18:39 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 encrypted_str = open("script" , 'r').read()
 key = os.environ.get('SECRET_KEY')
 
-fernet = Fernet(key.encode())
+fernet = Fernet(key.encode("utf-8"))
 decrypted = fernet.decrypt(encrypted_str.encode()).decode()
 
 decrypted_file = open("main.py" , 'w')
